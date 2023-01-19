6994d1a100d2d7a50193dde62673758822838fec
Judy-zheng <yuying.z@wustl.edu>
2023-01-19 14:03:17 -0600

    say hello

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -4,5 +4,6 @@ public class Message {
 
 	public static void main(String args[]) {
 		System.out.println("Hi, Judy~ I'm Yaxin Li from Olin WUSTL!");
+		System.out.println("Hi, Yaxin~ Nice to meet you!");
 	}
 }
