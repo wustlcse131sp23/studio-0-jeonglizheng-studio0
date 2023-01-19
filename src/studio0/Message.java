75cb358e22ab52a68db53d4da2bb5a2881dbf8e1
86139 <86139@Yaxin-Li.dhcp.wustl.edu>
2023-01-19 13:57:40 -0600

    Updated Message

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -3,6 +3,6 @@ package studio0;
 public class Message {
 
 	public static void main(String args[]) {
-		System.out.println("Hi, Judy! I'm Yaxin Li from Olin WUSTL!");
+		System.out.println("Hi, Judy~ I'm Yaxin Li from Olin WUSTL!");
 	}
 }
